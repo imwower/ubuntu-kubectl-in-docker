bc117a631a585f4396c91dc10511ffdb3f9bc509
im_wower <george674834080@gmail.com>
2018-09-19 10:34:07 +0800

    Add files via upload
    add set up script

---
+++ setup_kubectl.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+kubectl config set-cluster dev --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt --embed-certs=true --server="https://kubernetes.default/"
+kubectl config set-credentials user --token="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
+kubectl config set-context default --cluster=dev --user=user
+kubectl config use-context default
+echo "finished set up kubectl."
